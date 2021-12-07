dddb3344fa97594b682aa8f9fff89171e832e862
praveengit007 <95094438+praveengit007@users.noreply.github.com>
2021-12-07 19:20:30 +0530

    Update one.sh

--- one.sh
+++ one.sh
@@ -5,6 +5,6 @@
 
 
 I created dis for checking git push and pull, i.e, from local to remote then again remote to local
-Hey bro now i am creating a conflict in the same line which it is pushed to remote.07/12/2021
+07/12/2021 now i will create and resolve a conflict in pull request. Hey bro now i am creating a conflict in the same line which it is pushed to remote.07/12/2021
 
 
